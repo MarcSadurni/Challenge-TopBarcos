cecaf8212ffc38aeeedb820c016fe374c81fa98f
MarcSadurni <marcsadurni@LAPTOP-R7RNDFA3.localdomain>
2021-03-07 13:06:38 +0100

    arreglado funcionalidad

--- animations.js
+++ animations.js
@@ -1,25 +1,44 @@
-// const send= document.getElementById('send');
-// const modal_container= document.getElementById('modal_container');
-
-// send.addEventListener('click',()=>{
-//     modal_container.classList.add('show');
-// });
-
-function ajax(){
+function allInputsValid() {
+    const inputs = [...form.querySelectorAll('input')];
+    return inputs.every(input => input.checkValidity());
+  }
+  
+  function disableButton() {
+    send.disabled = true;
+  }
+  
+  function enableButton() {
+    send.disabled = false;
+  }
+  
+  function validateAndSubmit(e) {
+    e.preventDefault();
+    if (!allInputsValid()) return;
+    ajax(e);
+  }
+  
+  function ajax(_e){
+    disableButton();
     const http = new XMLHttpRequest();
     const url = 'http://date.jsontest.com/';
-
+  
     http.onreadystatechange = function (){
-        if (this.readyState == 4 && this.status == 200){
-            console.log(this.responseText);
-            document.getElementById('modal_container').innerHTML = this.responseText;
-        }
+      if (this.readyState == 4 && this.status == 200){
+        modalDate.innerText = JSON.parse(this.responseText).date
+        modal_container.classList.add('show');
+      } else {
+        enableButton();
+      }
     }
     http.open('GET', url);
     http.send();
-} 
-
-document.getElementById('send').addEventListener('click', function(){
-ajax();
-modal_container.classList.add('show');
-});
+  }
+  
+  const send = document.getElementById('send');
+  const modal_container= document.getElementById('modal_container');
+  const modalDate = modal_container.querySelector('.modal-date');
+  const name = document.getElementById('name');
+  const email = document.getElementById('email');
+  const obs = document.getElementById('obs');
+  const form = document.querySelector('.form-message');
+  form.addEventListener('submit', validateAndSubmit);
